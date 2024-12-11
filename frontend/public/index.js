ebf454b7e0de8b78700f009c001dc1c844b4382a
Andrey2720 <andrey_bashkirov2000@mail.ru>
2024-12-11 16:31:51 +0300

    сайтик

---
+++ frontend/public/index.js
@@ -0,0 +1,15 @@
+
+
+function sum(){
+    const first = Number(document.getElementById("first").value) 
+    const second = Number(document.getElementById("second").value)
+    const rezult = document.getElementById("rezult")
+    const rezult2 = document.getElementById("rezult2")
+
+    const s = first + second
+
+    rezult.innerHTML = s 
+
+    
+
+}
