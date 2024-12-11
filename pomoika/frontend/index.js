186b1e10e442d2c8d37168426a77d1202a5b39a7
Andrey2720 <andrey_bashkirov2000@mail.ru>
2024-12-11 15:54:57 +0300

    create pomoika

--- pomoika/frontend/index.js
+++ pomoika/frontend/index.js
@@ -4,10 +4,12 @@ function sum(){
     const first = Number(document.getElementById("first").value) 
     const second = Number(document.getElementById("second").value)
     const rezult = document.getElementById("rezult")
+    const rezult2 = document.getElementById("rezult2")
 
     const s = first + second
 
     rezult.innerHTML = s 
+
     
 
 }
